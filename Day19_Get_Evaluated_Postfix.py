0ddc8ebd831160bd5fc3666eed2d58d35fb8d2a0
Pavan Gawande <pavangawande824@gmail.com>
2024-09-29 22:23:41 +0530

    Update Day19_Get_Evaluated_Postfix.py

--- Day19_Get_Evaluated_Postfix.py
+++ Day19_Get_Evaluated_Postfix.py
@@ -6,12 +6,14 @@ def Get_Evaluated(s):
         else:
             if(i == '^'):
                 exp = str(int(l[-2])^int(l[-1]))
+            elif(i == '/'):
+                exp = str(int(int(l[-2])/int(l[-1])))
             else:
                 exp = str(l[-2])+i+str(l[-1])
             l = l[:-2]
             l.append(eval(exp))
     return l[0]
 
-s = input()
+s = ''.join(input().split())
 print(Get_Evaluated(s))
 
